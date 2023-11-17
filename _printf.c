ff8969d8850dc79da4dc8ff59c2d9236cd13e70d
FayezMohamed <fayezmfayez@gmail.com>
2023-11-17 11:40:38 -0800

    update

--- _printf.c
+++ _printf.c
@@ -1,48 +1,54 @@
 #include "main.h"
 
 /**
- * _printf - produces output according to a format
- * @format: specifiers
+ * _printf - Custom printf function
+ * @format: Format string
  *
  * Return: number of characters printed
  */
-
 int _printf(const char *format, ...)
 {
-	va_list ar;
-	int n = 0;
+    va_list ap;
+    int count = 0;
 
-	va_start(ar, format);
-	while (*format)
-	{
-		specs temp;
-		if (*format == '%')
-		{
-			format++;
-			if (*format == 'c')
-			{
-				temp.c = va_arg(ar, int);
-				_putchar(temp.c);
-				n++;
-			}
-			else if (*format == 's')
-			{
-				temp.s = va_arg(ar, char *);
-				while (*temp.s)
-				{
-					_putchar(*temp.s);
-					temp.s++;
-					n++;
-				}
-			}
-		}
-		else
-		{
-			_putchar(*format);
-			n++;
-		}
-		format++;
-	}
-	va_end(ar);
-	return (n);
+    va_start(ap, format);
+
+    while (format && *format)
+    {
+        if (*format == '%')
+        {
+            format++;
+            if (*format == '\0')
+                break; /* handle trailing '%' as you wish */
+
+            switch (*format)
+            {
+                case 'c':
+                    count += print_char(ap);
+                    break;
+                case 's':
+                    count += print_string(ap);
+                    break;
+                case '%':
+                    count += print_percent();
+                    break;
+                default:
+                    _putchar('%');
+                    _putchar(*format);
+                    count += 2;
+                    break;
+            }
+        }
+        else
+        {
+            _putchar(*format);
+            count++;
+        }
+
+        format++;
+    }
+
+    va_end(ap);
+
+    return count;
 }
