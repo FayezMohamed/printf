db80fc271be1bb84aef5acf5787406916e874ec6
FayezMohamed <fayezmfayez@gmail.com>
2023-11-17 12:25:34 -0800

    0

--- _printf.c
+++ _printf.c
@@ -8,49 +8,49 @@
  */
 int _printf(const char *format, ...)
 {
-    va_list ap;
-    int count = 0;
+    	va_list ap;
+    	int count = 0;
 
-    va_start(ap, format);
+    	va_start(ap, format);
 
-    while (format && *format)
-    {
-        if (*format == '%')
-        {
-            format++;
-            if (*format == '\0')
-            {
-                break; /* handle trailing '%' as you wish */
-            }
+    	while (format && *format)
+    	{
+        	if (*format == '%')
+        	{
+            		format++;
+            		if (*format == '\0')
+            		{
+                		break;
+            		}
 
-            switch (*format)
-            {
-                case 'c':
-                    count += print_char(ap);
-                    break;
-                case 's':
-                    count += print_string(ap);
-                    break;
-                case '%':
-                    count += print_percent();
-                    break;
-                default:
-                    _putchar('%');
-                    _putchar(*format);
-                    count += 2;
-                    break;
-            }
-        }
-        else
-        {
-            _putchar(*format);
-            count++;
-        }
+            		switch (*format)
+            		{
+                		case 'c':
+                    			count += print_char(ap);
+                    			break;
+                		case 's':
+                    			count += print_string(ap);
+                    			break;
+                		case '%':
+                    			count += print_percent();
+                    			break;
+                		default:
+                    			_putchar('%');
+                    			_putchar(*format);
+                    			count += 2;
+                    			break;
+            		}
+        	}
+        	else
+        	{
+            		_putchar(*format);
+            		count++;
+        	}
 
-        format++;
-    }
+        	format++;
+    	}
 
-    va_end(ap);
+    	va_end(ap);
 
-    return count;
+    	return count;
 }
